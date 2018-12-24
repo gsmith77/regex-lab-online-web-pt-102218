3adfd27d8db5c9b6123ec6630ebc543397fc9811
grant smith <grantsmith1256@gmail.com>
2018-12-24 01:39:01 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,4 +23,6 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
   end
 end
 
-def 
+def valid_phone_number?(text)
+  if text.match(/\?([0-9]{3})\? ([ -]?) ([0-9]{3}) ([ -]?) ([0-9]{4})/)
+end
